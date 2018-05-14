a84f078a5e3c63c305c453b748c6d06cb9ac91cc
Igor Gololobov <Igor.Gololobov@infowatch.com>
2018-05-14 14:29:45 +0300

    correct check style class

--- src/PhpWord/Writer/HTML/Style/Table.php
+++ src/PhpWord/Writer/HTML/Style/Table.php
@@ -16,17 +16,19 @@ class Table extends AbstractStyle
         $css = array();
 
 
-        $css['border-top-width'] = $this->getValueIf($style->getBorderTopSize() !== null, $style->getBorderTopSize().'px');
-        $css['border-top-color'] = $this->getValueIf($style->getBorderTopColor() !== null, '#'.$style->getBorderTopColor());
+        if ($style) {
+            $css['border-top-width'] = $this->getValueIf($style->getBorderTopSize() !== null, $style->getBorderTopSize() . 'px');
+            $css['border-top-color'] = $this->getValueIf($style->getBorderTopColor() !== null, '#' . $style->getBorderTopColor());
 
-        $css['border-right-width'] = $this->getValueIf($style->getBorderRightSize() !== null, $style->getBorderRightSize().'px');
-        $css['border-right-color'] = $this->getValueIf($style->getBorderRightColor() !== null, '#'.$style->getBorderRightColor());
+            $css['border-right-width'] = $this->getValueIf($style->getBorderRightSize() !== null, $style->getBorderRightSize() . 'px');
+            $css['border-right-color'] = $this->getValueIf($style->getBorderRightColor() !== null, '#' . $style->getBorderRightColor());
 
-        $css['border-bottom-width'] = $this->getValueIf($style->getBorderBottomSize() !== null, $style->getBorderBottomSize().'px');
-        $css['border-bottom-color'] = $this->getValueIf($style->getBorderBottomColor() !== null, '#'.$style->getBorderBottomColor());
+            $css['border-bottom-width'] = $this->getValueIf($style->getBorderBottomSize() !== null, $style->getBorderBottomSize() . 'px');
+            $css['border-bottom-color'] = $this->getValueIf($style->getBorderBottomColor() !== null, '#' . $style->getBorderBottomColor());
 
-        $css['border-left-width'] = $this->getValueIf($style->getBorderLeftSize() !== null, $style->getBorderLeftSize().'px');
-        $css['border-left-color'] = $this->getValueIf($style->getBorderLeftColor() !== null, '#'.$style->getBorderLeftColor());
+            $css['border-left-width'] = $this->getValueIf($style->getBorderLeftSize() !== null, $style->getBorderLeftSize() . 'px');
+            $css['border-left-color'] = $this->getValueIf($style->getBorderLeftColor() !== null, '#' . $style->getBorderLeftColor());
+        }
 
         return $this->assembleCss($css);
     }
