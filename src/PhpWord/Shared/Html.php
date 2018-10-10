8ee5c261a7e930f6b439dcc0a0be4078e5b7e780
Igor Gololobov <igor.gololobov@infowatch.ru>
2018-10-10 19:03:28 +0300

    correct break line html

--- src/PhpWord/Shared/Html.php
+++ src/PhpWord/Shared/Html.php
@@ -61,7 +61,7 @@ class Html
 
         // Preprocess: remove all line ends, decode HTML entity,
         // fix ampersand and angle brackets and add body tag for HTML fragments
-        $html = str_replace(array("\n", "\r"), '', $html);
+        //$html = str_replace(array("\n", "\r"), '', $html);
         $html = str_replace(array('&lt;', '&gt;', '&amp;'), array('_lt_', '_gt_', '_amp_'), $html);
         $html = html_entity_decode($html, ENT_QUOTES, 'UTF-8');
         $html = str_replace('&', '&amp;', $html);
